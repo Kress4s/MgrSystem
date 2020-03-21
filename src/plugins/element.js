158864be8a33aa9c58a256a09fac8be694beca58
York-xia <1208209000@qq.com>
2020-03-21 22:41:44 +0800

    实现首页的页面布局

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 // Message 需要全局挂载
 Vue.prototype.$message = Message
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <el-button type="info" @click="loginout">退出</el-button>
-  </div>
+  <el-container>
+    <!-- 头部区域 -->
+    <el-header>
+      <el-button type="info" @click="loginout">退出</el-button>
+    </el-header>
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧主体区 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -15,5 +24,18 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="scss" scoped>
+.el-container {
+  width: 100%;
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
