ad6a2a0faba0a71189120eba234a326768cfedaf
York-xia <1208209000@qq.com>
2020-03-21 10:09:21 +0800

    项目引入elementUI和项目的一些初始化工作

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  // store,
   render: h => h(App)
 }).$mount('#app')
