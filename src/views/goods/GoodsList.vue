69241a21aa8d3239d737322ca05749f2dffce98e
York-xia <1208209000@qq.com>
2020-04-07 20:24:48 +0800

    goodslist adds the function of deleting

--- src/views/goods/GoodsList.vue
+++ src/views/goods/GoodsList.vue
@@ -21,13 +21,31 @@
         <el-table-column label="商品重量" width="80px" prop="goods_weight"></el-table-column>
         <el-table-column label="创建时间" width="140px" prop="add_time"></el-table-column>
         <el-table-column label="操作" width="180px">
-          <template>
+          <template slot-scope="scope">
             <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+              @click="delgoods(scope.row.goods_id)"
+            >删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
+    <!-- 编辑的dialog -->
+    <!-- 编辑修改分类的dailog -->
+    <!-- <el-dialog title="修改商品" :visible.sync="editDialogVisible" width="30%">
+      <el-form :model="category" :rules="editCateFormRules" ref="ruleForm" label-width="100px">
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="category.cat_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editCate">确 定</el-button>
+      </span>
+    </el-dialog>-->
   </div>
 </template>
 
@@ -54,10 +72,27 @@ export default {
       const { data: res } = await this.$http.get('goods', { params: this.queryInfo })
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.goodsList = res.data.goods
-      console.log(this.goodsList)
+    },
+
+    async delgoods (goodsid) {
+      var comfirmResult = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+      if (comfirmResult === 'confirm') {
+        const { data: res } = await this.$http.delete('goods/' + goodsid)
+        if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+        this.$message.success('删除商品成功')
+        this.getGoodsList()
+      }
     }
   }
-
 }
 </script>
 
